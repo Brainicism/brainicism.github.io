b940c3acfbc65ed0e41813d5233f8d23fd1ec85c
Jay C <jayc.chowdhary@gmail.com>
2019-10-05 22:07:38 -0400

    Updated colors for Halloween theme.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ anime()
 
 function anime() {
     if(lazeez){
-        document.querySelector("h1").style.color = "yellow";
+        document.querySelector("h1").style.color = "orange";
         lazeez= false;
     }
     else{
-        document.querySelector("h1").style.color = "green";
+        document.querySelector("h1").style.color = "black";
         lazeez = true;
     }
 }
